fcd46caca61b054f539402c5a5de13bc3c027db9
sijilsasidharan <sijilsasidharan@gmail.com>
2025-01-01 00:46:31 +0530

    not found change

---
+++ my-app/src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const metadata = {
+  title: "Not Found",
+  description: "Page not found",
+};
+
+const NotFound = () => {
+  return <div>No Found</div>;
+};
+
+export default NotFound;
