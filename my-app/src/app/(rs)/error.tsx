683142bc4dd060e82ce022db9a86e468081f8ed3
sijilsasidharan <40237945+sijilsasidharan@users.noreply.github.com>
2025-01-02 23:55:19 +0530

    Update error.tsx
    Sentry update

--- my-app/src/app/(rs)/error.tsx
+++ my-app/src/app/(rs)/error.tsx
@@ -1,3 +1,25 @@
-export const Error () => {
-  return <>Error</>
+"use client"; // Error boundaries must be Client Components
+
+import * as Sentry from "@sentry/nextjs";
+import { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    Sentry.captureException(error);
+  }, [error]);
+
+  return (
+    // global-error must include html and body tags
+    <html>
+      <body>
+        <h2>Something went wrong!</h2>
+      </body>
+    </html>
+  );
 }
